76e2c00d1cd92182b8d3d87cbb4437dafb6997e1
Javed Hussain <tumhara-github-email@gmail.com>
2026-06-13 12:20:15 +0530

    update api

--- src/api/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const API=axios.create({baseURL:"http://localhost:5000/api"
-});
-export const createBooking=(data:any)=>API.post("/booking/create",data);
-export const getAllBookings=()=>API.get("/booking/all");
-export const deleteBooking=(id:string)=>API.delete(`/booking/delete/${id}`);
-export const adminLogin=(data:any)=>API.post("/admin/login",data);
